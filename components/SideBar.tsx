483f9fd1d05d04fd03aa8dedb9049fe66d546a46
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-01-20 22:02:22 +0100

    built sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
-import { CloseIcon, CloseIconContainer, SideBarContainer } from './SideBarElements';
+import { SideBarProps } from '../pages';
+import {
+  CloseIcon,
+  CloseIconContainer,
+  SideBarContainer,
+  SideBarLinkItem,
+  SideBarLinkContainer,
+  SideBarSignInButton,
+  SideBarSignInContainer,
+  SideBarLink,
+} from './SideBarElements';
 
-interface Props {
-  showSideBar: boolean;
-  toggleShowSideBar: () => void;
-}
-
-const SideBar = (props: Props) => {
+const SideBar = (props: SideBarProps) => {
   return (
     <SideBarContainer showSideBar={props.showSideBar}>
       <CloseIconContainer>
         <CloseIcon onClick={props.toggleShowSideBar} />
       </CloseIconContainer>
+      <SideBarLinkContainer>
+        <SideBarLinkItem>
+          <SideBarLink>About</SideBarLink>
+        </SideBarLinkItem>
+        <SideBarLinkItem>
+          <SideBarLink>Discover</SideBarLink>
+        </SideBarLinkItem>
+        <SideBarLinkItem>
+          <SideBarLink>Services</SideBarLink>
+        </SideBarLinkItem>
+        <SideBarLinkItem>
+          <SideBarLink>Sign Up</SideBarLink>
+        </SideBarLinkItem>
+      </SideBarLinkContainer>
+      <SideBarSignInContainer>
+        <SideBarSignInButton>Sign In</SideBarSignInButton>
+      </SideBarSignInContainer>
     </SideBarContainer>
   );
 };
