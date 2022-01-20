08e3435e088cacb15a6ab291af7f0a6e40f4aea3
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-01-20 22:02:02 +0100

    removed useEffect, added sidebar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useReducer, useEffect } from 'react';
 import HeroSection from '../components/HeroSection';
 import NavBar from '../components/NavBar';
-import styles from '../styles/Home.module.css';
+import SideBar from '../components/SideBar';
+
+export interface SideBarProps {
+  showSideBar: boolean;
+  toggleShowSideBar: () => void;
+}
 
 const Home: NextPage = () => {
+  const [showSideBar, toggleShowSideBar] = useReducer((s) => !s, false);
+
   return (
     <>
-      <NavBar />
+      <NavBar toggleShowSideBar={toggleShowSideBar} />
+      <SideBar showSideBar={showSideBar} toggleShowSideBar={toggleShowSideBar} />
       <HeroSection />
     </>
   );
