1ecb1146769455adf542009dedf9613914ef5644
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-01-20 22:01:04 +0100

    changed sign in button to next/link

--- components/NavElements.tsx
+++ components/NavElements.tsx
@@ -1,5 +1,7 @@
 import tw from 'tailwind-styled-components';
 import { HiMenu } from 'react-icons/hi';
+import { Link as LinkS } from 'react-scroll';
+import Link from 'next/link';
 
 export const Nav = tw.nav`
 h-20 
@@ -26,16 +28,23 @@ export function Logo({ children }: { children: string }) {
 }
 
 export const NavLinksContainer = tw.ul`
-hidden md:flex justify-between items-center h-full w-full max-w-md
+hidden md:flex justify-between items-center h-full w-full max-w-md first:pl-0 last:pr-0
 `;
 
-export const NavLink = tw.li`
-text-white
+export const NavLinkItem = tw.li`
 flex
 items-center
 justify-center
-text-xl
 px-3
+h-full
+`;
+
+export const NavLink = tw(LinkS)`
+flex
+text-white
+items-center
+justify-center
+text-xl
 first:cursor-pointer
 h-full
 hover:border-y-2
@@ -44,11 +53,15 @@ border-t-transparent
 rounded-b-sm
 `;
 
+export const RedNavLink = tw(NavLinkItem)`
+text-red-500
+`;
+
 export const SignInButtonContainer = tw.div`
 hidden md:block text-black text-xl
 `;
-
-export const SignInButton = tw.button`
+// const TG = (props: { gar: number }) => <div>{props.gar}</div>;
+export const SignInButton = tw(Link)`
 px-5 py-2 rounded-3xl bg-green-500 transition-all hover:bg-white
 `;
 
@@ -61,7 +74,7 @@ items-center
 `;
 
 export const SideBarMenu = tw(HiMenu)`
-
+font
 text-white
 cursor-pointer
 text-4xl
