15b6132343df279c3c7ba8ac57906703f98fbbba
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-01-27 22:00:03 +0100

    further built hero component

--- components/HeroElements.tsx
+++ components/HeroElements.tsx
@@ -1,9 +1,9 @@
 import tw from 'tailwind-styled-components';
 
-interface Props {}
+import { MdKeyboardArrowRight, MdArrowForward } from 'react-icons/md';
 
 export const HeroContainer = tw.div`
-h-[800px] -z-50 flex justify-center items-center relative`;
+h-[800px] z-10 flex justify-center items-center relative`;
 
 export const HeroBgContainer = tw.div`
 -z-[45] h-full w-full absolute inset-0
@@ -11,4 +11,28 @@ export const HeroBgContainer = tw.div`
 
 export const HeroVideoBg = tw.video`
 h-full w-full object-cover -z-40
+`;
+
+export const ArrowForward = tw(MdArrowForward)`
+ml-2
+text-xl
+`;
+
+export const ArrowRight = tw(MdKeyboardArrowRight)`
+ml-2
+text-xl`;
+
+export const HeroContent = tw.div`
+z-20 max-w-7xl absolute px-2 py-6 flex items-center
+`;
+export const HeroH1 = tw.h1`
+text-black text-center text-[32px] md:text-5xl
+`;
+export const HeroP = tw.p`
+text-black max-w-[600px] text-center text-[18px] md:text-2xl`;
+export const HeroBtnWrapper = tw.div`
+mt-8 flex items-center flex-col
+`;
+export const HeroBtn = tw.button`
+
 `;
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,15 +1,37 @@
 // import React from 'react'
 
-import { HeroBgContainer, HeroContainer, HeroVideoBg } from './HeroElements';
+import { useState } from 'react';
+import {
+  ArrowForward,
+  ArrowRight,
+  HeroBgContainer,
+  HeroBtn,
+  HeroBtnWrapper,
+  HeroContainer,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroVideoBg,
+} from './HeroElements';
 
 interface Props {}
 
 const HeroSection = (props: Props) => {
+  const [hover, setHover] = useState(false);
   return (
     <HeroContainer>
       <HeroBgContainer>
         <HeroVideoBg src="/video.mp4" typeof="video/mp4" autoPlay loop muted />
       </HeroBgContainer>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards your next payment
+        </HeroP>
+        <HeroBtnWrapper>
+          <HeroBtn>Get Started {hover ? <ArrowRight /> : <ArrowForward />}</HeroBtn>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 };
