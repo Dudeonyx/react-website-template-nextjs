66c987a55d93a68f8fcd34d40587825b7114e313
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-01-27 20:41:49 +0100

    basic classed for HeroElements

--- components/HeroElements.tsx
+++ components/HeroElements.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
 import tw from 'tailwind-styled-components';
 
 interface Props {}
 
 export const HeroContainer = tw.div`
-h-[800px] bg-black`;
+h-[800px] -z-50 flex justify-center items-center`;
 
-export const HeroBgContainer = tw.div``;
+export const HeroBgContainer = tw.div`
+-z-[45] h-full w-full absolute
+`;
 
-export const HeroVideoBg = tw.video``;
+export const HeroVideoBg = tw.video`
+h-full w-full object-cover -z-40
+`;
