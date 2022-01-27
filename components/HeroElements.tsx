5401ff5445efef1b9e3cc5c9a5f9dc97841e72a1
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-01-27 21:21:40 +0100

    tweaked hero position css

--- components/HeroElements.tsx
+++ components/HeroElements.tsx
@@ -3,10 +3,10 @@ import tw from 'tailwind-styled-components';
 interface Props {}
 
 export const HeroContainer = tw.div`
-h-[800px] -z-50 flex justify-center items-center`;
+h-[800px] -z-50 flex justify-center items-center relative`;
 
 export const HeroBgContainer = tw.div`
--z-[45] h-full w-full absolute
+-z-[45] h-full w-full absolute inset-0
 `;
 
 export const HeroVideoBg = tw.video`
