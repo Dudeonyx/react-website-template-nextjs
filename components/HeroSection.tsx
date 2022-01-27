c938fec964af9dc8d729c3a9d2cf53079ad90594
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-01-27 20:44:54 +0100

    added video import some video props

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection = (props: Props) => {
   return (
     <HeroContainer>
       <HeroBgContainer>
-        <HeroVideoBg />
+        <HeroVideoBg src="/video.mp4" typeof="video/mp4" autoPlay loop muted />
       </HeroBgContainer>
     </HeroContainer>
   );
