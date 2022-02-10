6783ff302c31e68d23044309e91595d9c3b77a5b
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-02-10 22:36:36 +0100

    update tw to latest

---
+++ components/InfoSection/Data.ts
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  darkText: false,
+  darkTextDesc: false,
+  topLine: 'Premium Bank',
+  headLine:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/svg.svg'),
+};
