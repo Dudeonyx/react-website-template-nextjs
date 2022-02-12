80b589008e9375635854fee3d5064740621cd82e
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-02-13 00:06:38 +0100

    added more date objects

--- components/InfoSection/Data.ts
+++ components/InfoSection/Data.ts
@@ -1,12 +1,51 @@
 export const homeObjOne = {
   id: 'about',
   lightBg: false,
-  darkText: false,
-  darkTextDesc: false,
+  lightText: true,
+  lightTextDesc: true,
   topLine: 'Premium Bank',
-  headLine:
+  headLine: 'Unlimited Transactions with zero fees',
+  description:
     'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
   buttonLabel: 'Get Started',
   imgStart: false,
   img: require('../../public/undraw_off_road_-9-oae.svg'),
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headLine: 'Login to your account at any time',
+  description:
+    'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
+  buttonLabel: 'Learn More',
+  imgStart: true,
+  img: require('../../public/undraw_savings_re_eq4w.svg'),
+  alt: 'Piggybank',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Join our Team',
+  headLine: 'Creating an account is extremely easy.',
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: 'Start Now',
+  imgStart: false,
+  img: require('../../public/undraw_secure_login_pdn4.svg'),
+  alt: 'paper',
+  dark: true,
+  primary: true,
+  darkText: false,
 };
