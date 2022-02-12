c16fd57a9cbab7f2c27500aa8264547288957473
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-02-12 22:11:11 +0100

    added data

--- components/InfoSection/Data.ts
+++ components/InfoSection/Data.ts
@@ -8,5 +8,5 @@ export const homeObjOne = {
     'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
   buttonLabel: 'Get Started',
   imgStart: false,
-  img: require('../../images/svg.svg'),
+  img: require('../../public/undraw_off_road_-9-oae.svg'),
 };
