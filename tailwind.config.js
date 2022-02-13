3e56f816c8242993528af2790287e92a804b7297
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-02-13 16:03:31 +0100

    added 'xs' screen size to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./pages/**/*.{js,jsx,ts,tsx,html}', './components/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
+    screens: {
+      xs: '480px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
